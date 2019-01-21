1e62139a44deecc2aea40f7ff78c827ee9d3ba26
yaroslav <ya.yuzvenko@gmail.com>
2019-01-21 22:21:54 +0200

    Updates

---
+++ precache-manifest.70a3efb0c9d65b78a3fb07bc9cd2852b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ce9e417a577bd34321bfc3d8788f258",
+    "url": "/lun-profile/static/media/dog4.2ce9e417.jpg"
+  },
+  {
+    "revision": "dcdd9149e72bee0b19d4a6a2debb70d1",
+    "url": "/lun-profile/static/media/cat3.dcdd9149.jpg"
+  },
+  {
+    "revision": "ba3540085227e18609af43ba762d7732",
+    "url": "/lun-profile/static/media/cat2.ba354008.jpg"
+  },
+  {
+    "revision": "1bf0ea9ad9527492f07e96c7ae2d894c",
+    "url": "/lun-profile/static/media/cat1.1bf0ea9a.jpg"
+  },
+  {
+    "revision": "974493b78be8622400cf",
+    "url": "/lun-profile/static/js/runtime~main.974493b7.js"
+  },
+  {
+    "revision": "4ae66712ec81ce93a386",
+    "url": "/lun-profile/static/js/main.4ae66712.chunk.js"
+  },
+  {
+    "revision": "a2b33bf3406ea8c6b82a",
+    "url": "/lun-profile/static/js/1.a2b33bf3.chunk.js"
+  },
+  {
+    "revision": "4ae66712ec81ce93a386",
+    "url": "/lun-profile/static/css/main.454f36b4.chunk.css"
+  },
+  {
+    "revision": "bde7f21509b878c9c69ba72189edbb1d",
+    "url": "/lun-profile/index.html"
+  }
+];
